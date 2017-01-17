39e6ff604cce5833d2c278502c95e5f986ff9245
Dieudonne Mukunzi <dmukunzi0@gmail.com>
2017-01-17 14:07:16 +0200

    added basic dynamic contents to pages controllers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,14 +4,26 @@ class PagesController < ApplicationController
 	end
 
 	def fortune
+
+		fortunes = ["You will be Teaching a Coding class next year!", "you will be a beggar!!", "you shall not live forver!! Duhh!", "You shall build the next facebook"]
+
+		@your_fortune = fortunes.sample
 		
 	end
 
 	def lottery
+
+		lucky_numbers = [rand(15..99)]
+
+		@your_luck_number = lucky_numbers.sample
 		
 	end
 
 	def impressions
+
+		views = [rand(20000..99999)]
+
+		@your_views = views.sample
 		
 	end
 end
